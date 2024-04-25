41c994888d749aa7c1cad9cd9bb0e7ac01421757
DCTP-BE <akpata.thelma@gmail.com>
2024-04-25 11:39:35 +0100

    v1

--- src/controllers/createCustomerId.js
+++ src/controllers/createCustomerId.js
@@ -1,6 +1,33 @@
 const createCustomerId = (req, res) => {
 
+    const createCIF = async (req, res) => {
+        try {
+            const {
+                date,
+                BVN,
+                title,
+                firstName,
+                middleName,
+                lastName,
+                DOB,
+                email,
+                phoneNumber,
+                address,
+                occupation,
+                gender,
+                maritalStatus,
+                country,
+            } = req.body;
 
+        } catch (error) {
+            console.error('Error generating customer ID:', error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    }
+    // Concatenate full name if middleName is present
+    const fullName = middleName
+        ? `${firstName} ${middleName} ${lastName}`
+        : `${firstName} ${lastName}`;
     // Generate a random customer ID
     const customerId = Math.floor(100000 + Math.random() * 900000);
 
