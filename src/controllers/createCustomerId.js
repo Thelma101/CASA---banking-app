e53463fb2bb9afa51166325fd161b64958b4c7d7
DCTP-BE <akpata.thelma@gmail.com>
2024-04-27 09:17:59 +0100

    v1

--- src/controllers/createCustomerId.js
+++ src/controllers/createCustomerId.js
@@ -1,4 +1,4 @@
-const customerDB = require('../models/cifDatabase'); // Ensure correct path
+const customerDB = require('../models/cifDatabase');
 const createError = require('http-errors');
 
 const createCustomerId = async (req, res, next) => {
