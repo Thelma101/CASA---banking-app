9fb5d0c9bd60185196bd417117f2ef52134dfc1b
DCTP-BE <akpata.thelma@gmail.com>
2024-04-27 20:13:31 +0100

    v2-transaction-fix

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 const createCurrentAccount = require('../controllers/createCurrentAccount');
 const createSavingsAccount = require('../controllers/createSavingsAccount');
+const transaction = require('../controllers/transaction');
 
 
 router.post('/', customerController.createCustomer);
 router.post('/', createCurrentAccount.createCurrentAccount);
 router.post('/', createSavingsAccount.createSavingsAccount);
+router.post('./transaction', transaction.transaction);
 
 module.exports = router;
