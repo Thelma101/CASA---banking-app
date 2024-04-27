73fd0ebf126366f6e862fe34b1a3a9985099dc99
DCTP-BE <akpata.thelma@gmail.com>
2024-04-27 22:28:30 +0100

    v1-account-closure-fix

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -4,11 +4,13 @@ const customerController = require('../controllers/customerController');
 const createCurrentAccount = require('../controllers/createCurrentAccount');
 const createSavingsAccount = require('../controllers/createSavingsAccount');
 const transaction = require('../controllers/transaction');
+const closeAccount = require('../controllers/closeAccount');
 
 
 router.post('/', customerController.createCustomer);
 router.post('/', createCurrentAccount.createCurrentAccount);
 router.post('/', createSavingsAccount.createSavingsAccount);
-router.post('./transaction', transaction.transaction);
+router.post('./', transaction.transaction);
+router.post('./', closeAccount.closeAccount);
 
 module.exports = router;
